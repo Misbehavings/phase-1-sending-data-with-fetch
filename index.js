d3000cd7a6366d6218dec622cb0edf31f2afccdd
Yun Chen <Yunchenfly@gmail.com>
2022-12-14 16:16:06 -0500

    commit tree aa511de07553b09100a167936fa49c12f3de8e82

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+let body = document.querySelector("body")
+function submitData (name, email) {
+    const data = {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        }),
+    }
+    return fetch('http://localhost:3000/users', data)
+    .then(resp => resp.json())
+    .then((obj) => {
+        let div = document.createElement('div')
+        div.textContent = obj.id
+        body.appendChild(div)
+    })
+    .catch((error)=> {
+        let h1 = document.createElement('h1')
+        h1.textContent = error.message
+        body.appendChild(h1)
+    })
+}
+
+
+
